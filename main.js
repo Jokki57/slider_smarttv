19f851ed2721adbc591f4b08cdfee073f4f9a920
Jurij <yurastein@gmail.com>
2018-01-24 09:19:02 +0200

    transition

--- main.js
+++ main.js
@@ -11,14 +11,43 @@ function generatePanel() {
 }
 
 var PANELS_COUNT = 6;
+var DELTA = 50;
 var panels = [];
 
 var panel;
-var root = document.getElementsByClassName('root')[0];
+var container = document.createElement('div');
+container.className = 'container';
+document.getElementsByClassName('root')[0].appendChild(container);
 for (var i = 0; i < PANELS_COUNT; ++i) {
   panel = generatePanel();
   panels.push(panel);
-  root.appendChild(panel);
+  container.appendChild(panel);
+}
+
+
+window.addEventListener('keydown', function(event) {
+  if (event.keyCode === 39) { // right
+    setTranslate(container, DELTA);
+  } else if (event.keyCode === 37) { // left
+    setTranslate(container, -DELTA);
+  }
+}, false);
+
+
+function setTranslate(elem, delta) {
+  if (elem) {
+    if (elem.style.transform) {
+      elem.style.transform = 'translateX(' + (getTranslateOffset(elem) + delta) + 'px)';
+    } else {
+      elem.style.transform = 'translateX(' + delta + 'px)'
+    }
+  }
+}
+
+function getTranslateOffset(elem) {
+  if (elem) {
+    return Number(elem.style.transform.match(/-?\d+/)[0]);
+  }
 }
 
 
